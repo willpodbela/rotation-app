c0f09d87033414510d5027e560c34466c79f0aa5
Zachary Podbela <podbelaapps@gmail.com>
2017-11-25 17:04:45 -0500

    Bug fix with mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   # Config mailer
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'rotation-app.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'rotation-app.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
     :address    => "smtp.sendgrid.net",
     :port       => 587,
