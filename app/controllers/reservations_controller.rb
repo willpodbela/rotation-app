b46ddd40f5fc9f7ae90d47268e509e0320205e74
Zachary Podbela <podbelaapps@gmail.com>
2018-03-03 03:38:15 -0500

    Reservation integration into the Web UI admin console as a nested route under Items

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,63 @@
+class ReservationsController < ApplicationController
+  before_action :set_item
+
+  def index
+    @reservations = @item.reservations
+  end
+
+  def show
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def new
+    @reservation = Reservation.new(:item_id => @item_id)
+  end
+  
+  def create
+    # Instantiate a new object using form parameters
+    @reservation = Reservation.new(reservation_params)
+    @reservation.item = @item
+    # Save the object
+    if @reservation.save
+      # If save succeeds, redirect to the index action
+      flash[:notice] = "Reservation created successfully."
+      redirect_to(item_reservations_path(@item))
+    else
+      # If save fails, redisplay the form so user can fix problems
+      render('new')
+    end
+  end
+
+  def edit
+    @reservation = Reservation.find(params[:id])
+  end
+  
+  def update
+    # Find a new object using form parameters
+    @reservation = Reservation.find(params[:id])
+    @reservation.item = @item
+    # Update the object
+    if @reservation.update_attributes(reservation_params)
+      # If save succeeds, redirect to the show action
+      flash[:notice] = "Reservation updated successfully."
+      redirect_to(item_reservation_path(@item, @reservation))
+    else
+      # If save fails, redisplay the form so user can fix problems
+      render('edit')
+    end
+  end
+  
+  def destroy
+    # TODO render 404.
+  end
+  
+  private
+  
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date, :user_id, :status)
+  end
+  
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+end
