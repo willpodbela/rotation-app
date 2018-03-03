7250fad55293622ed0722d3b6d1a7ce855590f90
Barron Roth <barronlroth@gmail.com>
2018-03-03 15:22:25 -0500

    add image to item with conversion options on upload

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,18 @@
 class Item < ApplicationRecord
+	has_attached_file :image, 
+		url: "/system/:hash.:extension",
+		hash_secret: "longSecretString",
+		styles: {
+			thumb: ["300x300#", :jpeg],
+			original: [:jpeg]
+		},
+		convert_options: {
+			thumb: "-quality 70 -strip",
+			original: "-quality 90"
+		},
+
+
+	validates_attachment :image,
+		content_type: { content_type: /\Aimage\/.*\z/ },
+        size: { less_than: 10.megabyte }
 end
