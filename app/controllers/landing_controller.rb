20fce0780e99f1aa6feb309144c0186d057258b2
Zachary Podbela <podbelaapps@gmail.com>
2018-04-19 23:28:47 -0400

    Modularized theme to devise and landing pages via layouts

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,6 +2,8 @@ class LandingController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :status, :sign_up]
   before_action :enforce_access_control_admin!, only: [:admin]
   
+  layout "devise"
+  
   # Home Page
   def index
   end
