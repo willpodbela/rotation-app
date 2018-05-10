ad730b9d9265e93f53119107766e560310f0b457
Zachary Podbela <podbelaapps@gmail.com>
2018-05-09 22:58:29 -0400

    Ran migrations to update shcema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180507235846) do
+ActiveRecord::Schema.define(version: 20180510024835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "devices", force: :cascade do |t|
+    t.string "token", limit: 64, null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["token", "user_id"], name: "index_devices_on_token_and_user_id"
+    t.index ["token"], name: "index_devices_on_token", unique: true
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "title", null: false
