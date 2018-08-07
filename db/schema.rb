7cb68ebfdde0d4bfb4a8925c584b8d3888adbfc6
Zachary Podbela <podbelaapps@gmail.com>
2018-08-06 23:11:51 -0400

    Added column buyURL to Items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180513192001) do
+ActiveRecord::Schema.define(version: 20180807030423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20180513192001) do
     t.string "subtitle"
     t.string "retail_value"
     t.string "color"
+    t.string "buyURL"
   end
 
   create_table "profiles", force: :cascade do |t|
