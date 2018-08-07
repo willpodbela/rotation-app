00a81e4a877bab0f3f0f1ada893aa324c0d1e445
Zachary Podbela <podbelaapps@gmail.com>
2018-08-06 23:16:03 -0400

    Temporarily diabling authentication on endpoint. DO NOT ELEVATE TO PROD

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,7 +1,7 @@
 module Api
     module V1
     class ItemsController < Api::V1::BaseController
-      http_basic_authenticate_with name:ENV["API_AUTH_NAME"], password:ENV["API_AUTH_PASSWORD"], only: [:create]
+      #http_basic_authenticate_with name:ENV["API_AUTH_NAME"], password:ENV["API_AUTH_PASSWORD"], only: [:create]
       skip_before_action :authenticate_user_from_token!, only: [:create]
       before_action :set_current_user, except: [:create]
       
