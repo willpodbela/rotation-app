d8e808f25026b257a7b60c7b4227591fce12042e
Zachary Podbela <podbelaapps@gmail.com>
2018-09-11 23:02:22 -0400

    Added code to create profile object for all users after save if they don't already have one. This is safer than creating a profile object 'when needed' as we have been doing (we currently have code that calls to create profile on first call to profile view endpoint)

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,10 @@ class User < ApplicationRecord
   
   before_save :ensure_authentication_token
   
+  after_save do |user|
+    user.create_profile unless user.profile.present?
+  end
+  
   enum access_level: [ :waitlist, :standard, :admin ]
   
   def ensure_authentication_token
