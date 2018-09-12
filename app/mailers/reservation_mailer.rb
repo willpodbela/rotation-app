772f6e59912c1e1414e04c4bbacb4a7af438898f
Zachary Podbela <podbelaapps@gmail.com>
2018-09-11 22:38:55 -0400

    Moved message body into Mailer view template

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -10,33 +10,13 @@ class ReservationMailer < ActionMailer::Base
   end
   
   def reservation_created(reservation)
-    item = reservation.item
-    user = reservation.user
-    profile = user.profile
-    
-    body = "A new reservation has been created.\n\n"
-    body += "<ul>Item</ul>\n"
-    body += "ID: #{item.id}\n"
-    body += "Brand: #{item.title}\n"
-    body += "Name: #{item.subtitle}\n\n"
-    if item.company_owned?
-      body += "Item is company owned."
-    else
-      body += "<a href='#{item.buyURL}'>Purchase Item</a>"
-    end
-    
-    body += "\n\n<ul>User</ul>\n"
-    body += "ID: #{user.id}\n"
-    body += "Email: #{user.email}\n"
-    body += "Address Line 1: #{profile.address_line_one}\n"
-    body += "Address Line 2: #{profile.address_line_two}\n"
-    body += "Address City: #{profile.address_city}\n"
-    body += "Address State: #{profile.address_state}\n"
-    body += "Address Zip: #{profile.address_zip}"
+    @item = reservation.item
+    @user = reservation.user
+    @profile = @user.profile
     
     @subject += "New Reservation!"
     
-    mail(to: @to_address, body: body, content_type: "text/html", subject: @subject)
+    mail(to: @to_address, subject: @subject)
   end
   
   def reservation_destroyed(reservation)
