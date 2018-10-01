11929e49a09eec2f6bc2633611c1fd49956fe5eb
Zachary Podbela <podbelaapps@gmail.com>
2018-10-01 19:40:12 -0400

    trying new syntax

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,12 +1,10 @@
-require 'houston'
-
 class Device < ApplicationRecord
   belongs_to :user
   
   def send_notification(message)   
     n = Houston::Notification.new(device: self.token)
     n.alert = message
-    APN.push(n)
+    Houston::Client.development.push(n)
     
     # n = Rpush::Apns::Notification.new
 #     n.app = Rpush::Apns2::App.find_by_name("rotation_ios")
