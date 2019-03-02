3cca5f0734dfc31937acbebfb7da3f6c2e05980e
Zachary Podbela <podbelaapps@gmail.com>
2019-03-02 13:05:34 -0500

    Refactored existing web flow to leverage new ServiceObject

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,42 +1,22 @@
  class SubscriptionsController < ApplicationController
   
   def create
-    Stripe.api_key = Rails.configuration.stripe[:secret_key]
-
-    if current_user.stripe_customer_id? 
-      customer = Stripe::Customer.retrieve(current_user.stripe_customer_id)
-    else
-      customer = Stripe::Customer.create(
-        :email => subscription_params[:stripeEmail],
-        :source  => subscription_params[:stripeToken]
-      )
-      current_user.stripe_customer_id = customer.id
-      unless current_user.save
-        # TODO: Log error
-      end
-    end
-    
-    stripe_subscription_obj = customer.subscriptions.create({plan: stripe_plan_id})
-    
-    response = ServiceResponse.new(stripe_subscription_obj)
-    if response.success?
-      flash[:notice] = 'You have successfully subscribed to our premium plan!'
-      subsciption = Subscription.new(
-        :user => current_user,
-        :stripe_subscription_obj => stripe_subscription_obj,
-        :stripe_plan_id => stripe_plan_id
-      )
-      unless subsciption.save
-        # Log this error.
-      end
+    begin
+      StripeService.create_monthly_subscription(current_user, subscription_params[:stripeToken])
       
       if session[:ios_init]
         session[:ios_init] = false
         redirect_to ios_deep_link_path
       else 
+        flash[:notice] = 'You have successfully subscribed to our premium plan!'
         redirect_to status_path
       end
-    else
+    rescue Stripe::CardError => e
+      # CardError; return the error message.
+      flash[:alert] = e.message
+      redirect_to status_path
+    rescue => e
+      # Some other error
       flash[:alert] = 'Ooops, something went wrong!'
       redirect_to status_path
     end
