8fd2725151db9fe5293e26a9cc545e36d50eef68
Zachary Podbela <podbelaapps@gmail.com>
2019-03-02 16:57:00 -0500

    HUGE bug where I was not running subscription code if the customer was already registered with stripe

--- app/services/stripe_service.rb
+++ app/services/stripe_service.rb
@@ -6,6 +6,7 @@ class StripeService
       
       Stripe.api_key = Rails.configuration.stripe[:secret_key]
       
+      # Set customer (retrive or create)
       if user.stripe_customer_id? 
         customer = Stripe::Customer.retrieve(user.stripe_customer_id)
       else
@@ -15,28 +16,27 @@ class StripeService
         )
         user.stripe_customer_id = customer.id
         unless user.save
+          # TODO: Log error - stripe succeeded but local obj could not be saved          
+        end
+      end
+      
+      # Create subscription
+      stripe_subscription_obj = customer.subscriptions.create({plan: ENV['STRIPE_PLAN_ID']})
+      response = ServiceResponse.new(stripe_subscription_obj)
+      if response.success?
+        subsciption = Subscription.new(
+          :user => user,
+          :stripe_subscription_obj => stripe_subscription_obj,
+          :stripe_plan_id => ENV['STRIPE_PLAN_ID']
+        )
+        unless subsciption.save
           # TODO: Log error - stripe succeeded but local obj could not be saved
         else
-          
-          # Happy path
-          stripe_subscription_obj = customer.subscriptions.create({plan: ENV['STRIPE_PLAN_ID']})
-          response = ServiceResponse.new(stripe_subscription_obj)
-          if response.success?
-            subsciption = Subscription.new(
-              :user => user,
-              :stripe_subscription_obj => stripe_subscription_obj,
-              :stripe_plan_id => ENV['STRIPE_PLAN_ID']
-            )
-            unless subsciption.save
-              # TODO: Log error - stripe succeeded but local obj could not be saved
-            else
-              return subsciption
-            end
-          else
-            # TODO: Log error from stripe
-            raise StandardError.new("Unable to create subscription in Stripe")
-          end
+          return subsciption
         end
+      else
+        # TODO: Log error from stripe
+        raise StandardError.new("Unable to create subscription in Stripe")
       end
     end
     
