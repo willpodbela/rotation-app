c76e7309da5d0a66bcad3d431b242ea0a44b0e91
Zachary Podbela <podbelaapps@gmail.com>
2019-04-07 21:30:50 -0400

    Changed enum :active state to :available state

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,14 +1,14 @@
 class Unit < ApplicationRecord
   belongs_to :item
   
-  scope :available_for_rent, -> { where(status: [:in_transit_from_supplier, :active]) }
+  scope :available_for_rent, -> { where(status: [:in_transit_from_supplier, :available]) }
   
   enum size: [ :S, :M, :L, :XL ]
-  enum status: [ :pending, :in_transit_from_supplier, :active, :sold, :returned, :retired ]
+  enum status: [ :pending, :in_transit_from_supplier, :available, :sold, :returned, :retired ]
   
   before_save do |unit|
     # If unit is sold, returned, or retired, set its retire_date
-    unit.retire_date = Date.today if ((["pending", "in_transit_from_supplier", "active"].include? unit.status_was) && (["sold", "returned", "retired"].include? unit.status))
+    unit.retire_date = Date.today if ((["pending", "in_transit_from_supplier", "available"].include? unit.status_was) && (["sold", "returned", "retired"].include? unit.status))
   end
   
 end
