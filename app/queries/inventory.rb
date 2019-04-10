972c3fc67a874051f0dd5f4b5381c9acccb1a9ad
Zachary Podbela <podbelaapps@gmail.com>
2019-04-10 16:48:07 -0400

    Enhanced Inventory Query Object

--- app/queries/inventory.rb
+++ app/queries/inventory.rb
@@ -2,6 +2,7 @@ module Queries
   class Inventory
     def initialize
       available_sizes = Unit.group(:item_id, :size).count
+      @total_count_by_item = Unit.group(:item_id).count
       @sizes = Hash.new
 
       available_sizes.each do |key, value|
@@ -11,11 +12,15 @@ module Queries
     end
 
     def size_availability(item)
-      @sizes[item.id]
+      @sizes[item.id] || Hash.new
     end
 
     def num_availabile(item, size)
       @sizes[item.id][size] || 0
+    end
+    
+    def total_available(item)
+      @total_count_by_item[item.id] || 0
     end
   end
 end
