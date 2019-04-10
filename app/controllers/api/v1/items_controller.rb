11839778729e10910cd14b2e09bac2e35fe0b625
Zachary Podbela <podbelaapps@gmail.com>
2019-04-10 16:35:55 -0400

    Removed dead code that wasn't being run

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -3,8 +3,7 @@ module Api
     class ItemsController < Api::V1::BaseController
       #http_basic_authenticate_with name:ENV["API_AUTH_NAME"], password:ENV["API_AUTH_PASSWORD"], only: [:create]
       skip_before_action :authenticate_user_from_token!, only: [:create]
-      before_action :set_current_user, except: [:show, :index]
-      
+            
       #Failsafe: Override endpoints that we don't want to make available
       def destroy
         render_error(405)
@@ -122,13 +121,6 @@ module Api
     
       def display_params
         params.permit(:sort_by_section)
-      end
-    
-      def set_current_user
-        # Re-fetch current_user with eager_load
-        current_user = User.eager_load(:up_next_items).eager_load(:my_rotation_items).eager_load(:live_reservations).eager_load(:scheduled_reservations).order(:id).find(current_user.id)
-        # Set instance variable for use in views
-        @current_user = current_user
       end
       
     end
