27598863b24569ca35ac660d4c88f2bda2652b25
Zachary Podbela <podbelaapps@gmail.com>
2019-04-24 00:18:00 -0400

    Added entire reservation object to returned item json dictionary

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,4 +1,13 @@
 module ItemsHelper
+  def reservation(user, item)
+    res = @current_user.live_reservations.select {|s| s.item_id == item.id}.first
+    res ||= @current_user.scheduled_reservations.select {|s| s.item_id == item.id}.first
+    return res.nil? ? nil : res.attributes
+  end
+
+  # START: DEPRECATED CODE BLOCK
+  # These functions are deprecated. This code (and calling code in _item.json.jbuilder) should be removed in future versions
+
   def my_rotation_reservation_id(user, item)
     res = @current_user.live_reservations.select {|s| s.item_id == item.id}.first
     return res.nil? ? nil : res.id
@@ -8,4 +17,6 @@ module ItemsHelper
     res = @current_user.scheduled_reservations.select {|s| s.item_id == item.id}.first
     return res.nil? ? nil : res.id
   end
+  
+  # END: DEPRECATED CODE BLOCK
 end
--- app/views/api/v1/items/_item.json.jbuilder
+++ app/views/api/v1/items/_item.json.jbuilder
@@ -3,6 +3,9 @@ json.my_rotation                  @current_user.my_rotation_items.include? item
 json.my_rotation_reservation_id   my_rotation_reservation_id(@current_user, item)
 json.up_next                      @current_user.up_next_items.include? item
 json.up_next_reservation_id       up_next_reservation_id(@current_user, item)
+json.reservation do
+  json.merge!                       reservation(@current_user, item)
+end
 json.image_url      item.image.url
 json.image_url_small      item.image.url(:small)
 json.image_url_large      item.image.url(:large)
