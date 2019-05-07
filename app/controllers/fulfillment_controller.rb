3ad0d4ad513f3171ad30f8958165c60ca51d10b5
Zachary Podbela <podbelaapps@gmail.com>
2019-05-06 23:20:52 -0400

    refactored code to be more elegant and DRY

--- app/controllers/fulfillment_controller.rb
+++ app/controllers/fulfillment_controller.rb
@@ -1,6 +1,5 @@
 class FulfillmentController < ApplicationController
   def index
-    @outbound_reservations = Reservation.where(status: [:scheduled, :processing]).order(:status)
-    @active_reservations = Reservation.where(status: [:active, :returned]).order(:status)
+    @reservations = Reservation.where(status: [:scheduled, :processing, :active, :returned]).order(:status)
   end
 end
