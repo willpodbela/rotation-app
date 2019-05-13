606436871cf7bee2ad73a921e5e85b0f034a8b1b
Zachary Podbela <podbelaapps@gmail.com>
2019-05-13 19:13:00 -0400

    quick changes based on Zayns feedback

--- app/controllers/fulfillment_controller.rb
+++ app/controllers/fulfillment_controller.rb
@@ -1,7 +1,7 @@
 class FulfillmentController < AdminBaseController
   def index
     @reservations = Reservation
-    .includes(:unit, :user)
+    .includes(:unit, :user, :item)
     .where(status: [:scheduled, :processing, :active, :returned])
     .order("#{sort_column} #{sort_direction}")
   end
@@ -9,7 +9,7 @@ class FulfillmentController < AdminBaseController
   private
   
   def sortable_columns
-    ["status", "created_at", "unit_id", "units.notes", "users.email", "id", "size"]
+    ["status", "created_at", "unit_id", "units.notes", "users.email", "id", "size", "items.title"]
   end
   
 end
