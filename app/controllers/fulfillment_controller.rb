20e04c7ba12999d7283e4535054278d4333219f8
Zachary Podbela <podbelaapps@gmail.com>
2019-05-13 19:04:27 -0400

    Added sorting for columns on associated models

--- app/controllers/fulfillment_controller.rb
+++ app/controllers/fulfillment_controller.rb
@@ -1,12 +1,15 @@
 class FulfillmentController < AdminBaseController
   def index
-    @reservations = Reservation.where(status: [:scheduled, :processing, :active, :returned]).order("#{sort_column} #{sort_direction}")
+    @reservations = Reservation
+    .includes(:unit, :user)
+    .where(status: [:scheduled, :processing, :active, :returned])
+    .order("#{sort_column} #{sort_direction}")
   end
   
   private
   
   def sortable_columns
-    ["status", "created_at"]
+    ["status", "created_at", "unit_id", "units.notes", "users.email", "id", "size"]
   end
   
 end
