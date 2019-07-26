f0d86cfaaedf8611630a5fb38bacfe65f6920f66
Zachary Podbela <podbelaapps@gmail.com>
2019-07-26 18:50:53 -0400

    Implimented update call when profile is changed to save first name, last name, and address to mailchimp

--- app/services/mail_chimp_service.rb
+++ app/services/mail_chimp_service.rb
@@ -3,7 +3,27 @@ class MailChimpService
     
     # Should be called when profile information is updated or re-tagging is needed
     def sync_and_tag(user)
-      gibbon_req.lists(list_id).members(subscriber_hash(user)).upsert(body: {email_address: user.email, status: "subscribed", merge_fields: {}})
+      merge_fields = {}
+      prof = user.profile
+      unless prof.nil?
+        unless prof.first_name.nil?
+          merge_fields[:FNAME] = prof.first_name
+        end
+        unless prof.last_name.nil?
+          merge_fields[:LNAME] = prof.last_name
+        end
+        unless prof.full_address.empty?
+          merge_fields[:ADDRESS] = {}
+          merge_fields[:ADDRESS][:addr1] = (prof.address_line_one || "")
+          merge_fields[:ADDRESS][:addr2] = (prof.address_line_two || "")
+          merge_fields[:ADDRESS][:city] = (prof.address_city || "")
+          merge_fields[:ADDRESS][:state] = (prof.address_state || "")
+          merge_fields[:ADDRESS][:zip] = (prof.address_zip || "")
+          merge_fields[:ADDRESS][:country] = "USA"
+        end
+      end
+      
+      gibbon_req.lists(list_id).members(subscriber_hash(user)).upsert(body: {email_address: user.email, status: "subscribed", merge_fields: merge_fields})
     end
     
     #private
@@ -19,6 +39,20 @@ class MailChimpService
     
     def subscriber_hash(user)
       Digest::MD5.hexdigest user.email.downcase
+    end
+    
+    #wip
+    
+    def get_tags(user)
+      gibbon_req.lists(list_id).members(subscriber_hash(user)).tags.retrieve
+    end
+    
+    def add_tags(user, tags=[])
+    
+    end
+    
+    def remove_tags(user, tags=[])
+    
     end
     
   end
