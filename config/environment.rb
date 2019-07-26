d796970e419354559806212792153cc03d5da667
Zachary Podbela <podbelaapps@gmail.com>
2019-07-26 16:11:10 -0400

    Added QA mailchimp API keys and envvariable

--- config/environment.rb
+++ config/environment.rb
@@ -10,4 +10,5 @@ unless Rails.env.production?
   ENV['INSTAGRAM_ENABLED'] = "true"
   ENV['EMAIL_PREFIX'] = "LOCAL DEV: "
   ENV['STRIPE_PLAN_ID'] = "plan_DmpGqUGCX1SpsS"
+  ENV['MAIL_CHIMP_API_KEY'] = "a128a2f0edefa01e9526b94fddaee383-us3"
 end
