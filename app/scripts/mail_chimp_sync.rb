27c52630066987b0d82f95d9454ddbdf5e1c3163
Zachary Podbela <podbelaapps@gmail.com>
2019-07-28 14:02:51 -0400

    Used find_in_batches instead of .all to combat crash due to max out of RAM utilization due to massive size of data query

--- app/scripts/mail_chimp_sync.rb
+++ app/scripts/mail_chimp_sync.rb
@@ -1,11 +1,11 @@
 module Scripts
   class MailChimpSync
     def self.sync_all
-      execution = []
-      for user in User.all.includes(:subscriptions, :profile, :referral_code, :advertisement_code, :current_valid_subscriptions).to_a
-        execution << MailChimpService.sync_and_tag(user)
+      @execution = []
+      User.includes(:subscriptions, :profile, :referral_code, :advertisement_code, :current_valid_subscriptions).find_in_batches do |group|
+        group.each { |user| @execution << MailChimpService.sync_and_tag(user) }
       end
-      return execution
+      return @execution
     end
   end
 end
