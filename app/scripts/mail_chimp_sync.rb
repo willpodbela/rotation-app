0d4a106e59c0c37d8d83b12050046005f71caba7
Zachary Podbela <podbelaapps@gmail.com>
2019-07-28 14:39:03 -0400

    Modified MailChimpSync to run in background / separate thread so as to not timeout terminal connection

--- app/scripts/mail_chimp_sync.rb
+++ app/scripts/mail_chimp_sync.rb
@@ -1,11 +1,19 @@
 module Scripts
   class MailChimpSync
-    def self.sync_all
-      execution = []
-      User.includes(:subscriptions, :profile, :referral_code, :advertisement_code, :current_valid_subscriptions).find_in_batches(batch_size: 200) do |group|
-        group.each { |user| execution << MailChimpService.sync_and_tag(user) }
+    require 'concurrent'
+  
+    def initialize
+      @execution = []
+    end
+    attr_reader :execution
+  
+    def sync_all
+      Concurrent::Future.execute do
+        User.includes(:subscriptions, :profile, :referral_code, :advertisement_code, :current_valid_subscriptions).find_in_batches(batch_size: 200) do |group|
+          group.each { |user| @execution << MailChimpService.sync_and_tag(user) }
+        end
       end
-      return execution
+      return "Sync Initialized. Call MailChimpSync.execution to query futures"
     end
   end
 end
