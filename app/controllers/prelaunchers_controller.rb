91f25917a70fa0ba5ba7426c6b63b9d44b71cd54
Zachary Podbela <podbelaapps@gmail.com>
2019-09-18 22:43:20 -0400

    added logic to handle logged in users

--- app/controllers/prelaunchers_controller.rb
+++ app/controllers/prelaunchers_controller.rb
@@ -3,13 +3,15 @@ class PrelaunchersController < ApplicationController
   layout "landing"
 
   def create
+    
+    
     @prelaunch_user = PrelaunchUser.find_by_email(prelaunch_user_params[:email])
     unless @prelaunch_user.nil?
-      prelaunch_user_success(@prelaunch_user)
+      create_success(@prelaunch_user)
     else
       @prelaunch_user = PrelaunchUser.new(prelaunch_user_params)
       if @prelaunch_user.save
-        prelaunch_user_success(@prelaunch_user)
+        create_success(@prelaunch_user)
       else
         # If save fails, redisplay the form so user can fix problems
         render('landing/index')
@@ -18,11 +20,20 @@ class PrelaunchersController < ApplicationController
   end
   
   def show
-    # Validate prelaunch_user has been set, otherwise return to landing page
-    @prelaunch_user = PrelaunchUser.find_by_email(cookies[:prelaunch_email])
+    # If user signed in, use that for invite page
+    if user_signed_in?
+      @prelaunch_user = PrelaunchUser.find_or_create_by(email: current_user.email)
+      cookies.permanent[:prelaunch_email] = prelaunch_user.email
+    end
+    
+    # If prelaunch_user is still nil, try to load prelaunch_user from cookie
+    @prelaunch_user ||= PrelaunchUser.find_by_email(cookies[:prelaunch_email])
+    
     if @prelaunch_user.nil?
+      # If still nil, send user to landing page
       redirect_to root_path
     else
+      # Else, prelaunch user is set, continue with render of invite page
       @friend_count = @prelaunch_user.invited_users.count
     end
   end
@@ -33,8 +44,14 @@ class PrelaunchersController < ApplicationController
     params.require(:prelaunch_user).permit(:email, :inviter_id)
   end
   
-  def prelaunch_user_success(prelaunch_user)
+  def create_success(prelaunch_user)
     cookies.permanent[:prelaunch_email] = prelaunch_user.email
+    
+    # This is the result of a click on submit on the landing form, so if user is signed in, sign them out.
+    if user_signed_in?
+      sign_out current_user
+    end
+    
     redirect_to(prelauncher_path)
   end
 end
