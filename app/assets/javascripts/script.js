53be7fc5c18539ffb2d7c9ee6c0209e4844044db
Zachary Podbela <podbelaapps@gmail.com>
2019-09-23 16:58:19 -0400

    Added copied tool tip for link copy

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -24,8 +24,35 @@ $( document ).ready(function() {
 // Clipboard.js
 
 $(document).ready(function(){  
-  var clipboard = new Clipboard('.clipboard-btn');	
+  var clipboard = new Clipboard('.clipboard-btn');
+  
+  clipboard.on('success', function(e) {
+    setTooltip(e.trigger, 'Copied!');
+    hideTooltip(e.trigger);
+  });
+
+  clipboard.on('error', function(e) {
+    setTooltip(e.trigger, 'Failed!');
+    hideTooltip(e.trigger);
+  });
 });
+
+$('.clipboard-btn').tooltip({
+  trigger: 'click',
+  placement: 'bottom'
+});
+
+function setTooltip(btn, message) {
+  $(btn).tooltip('show')
+    .attr('data-original-title', message)
+    .tooltip('show');
+}
+
+function hideTooltip(btn) {
+  setTimeout(function() {
+    $(btn).tooltip('hide');
+  }, 1000);
+}
 
 // Scroll.js
 
