5c1eb1b09024523aa2aaa23267ef1d4c0c4e09e5
Barron Roth <barronlroth@gmail.com>
2019-12-12 15:16:41 -0500

    syntax fix for SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -124,6 +124,3 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
 end
-
-# Config forced HTTPS for all application requests
-config.force_ssl = true
