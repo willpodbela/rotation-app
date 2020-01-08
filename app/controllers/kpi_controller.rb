1be27de91b3d2f2782cf8370cde6c08455240c3c
Zachary Podbela <podbelaapps@gmail.com>
2020-01-08 10:53:38 -0500

    Added customer distribution

--- app/controllers/kpi_controller.rb
+++ app/controllers/kpi_controller.rb
@@ -2,5 +2,12 @@ class KpiController < ApplicationController
   def index
     @visible_count = Item.visible.with_images.count
     @inventory = Queries::Inventory.new
+    
+    @customer_sizes = Hash.new
+    User.paying_customers.each do |u|
+      res_sizes = u.reservations.map(&:size)
+      size_freq = Hash[res_sizes.group_by{|x|x}.map{|k, v| [k, 1.0*v.size/res_sizes.length]}]
+      @customer_sizes.merge!(size_freq) {|k, o, n| o + n}
+    end
   end
 end
