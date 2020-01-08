5edf222ed03869fefdf67ecdcaf4247f79df9665
Zachary Podbela <podbelaapps@gmail.com>
2020-01-08 13:17:43 -0500

    Bug fix with inventory KPI cals

--- app/controllers/kpi_controller.rb
+++ app/controllers/kpi_controller.rb
@@ -5,7 +5,7 @@ class KpiController < ApplicationController
     
     @customer_sizes = Hash.new
     User.paying_customers.each do |u|
-      res_sizes = u.reservations.map(&:size)
+      res_sizes = u.reservations.map(&:size).compact
       size_freq = Hash[res_sizes.group_by{|x|x}.map{|k, v| [k, 1.0*v.size/res_sizes.length]}]
       @customer_sizes.merge!(size_freq) {|k, o, n| o + n}
     end
