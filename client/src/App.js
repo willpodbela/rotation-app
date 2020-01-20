8e95cf98b61917a4ba025dc0e2e6891d2e563059
Barron Roth <barronlroth@gmail.com>
2020-01-20 18:05:54 -0500

    add route for FAQ

--- client/src/App.js
+++ client/src/App.js
@@ -286,6 +286,7 @@ class App extends Component {
             }/>
             <Route path="/terms" exact component={TermsPage} />
             <Route path="/privacy" exact component={PrivacyPage} />
+            <Route path="/faq" exact component={FAQPage} />
             <Footer />
           </div>
         </Router>
