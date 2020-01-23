b597055cd1501c5770f9f99078157ab580620d40
Barron Roth <barronlroth@gmail.com>
2020-01-23 11:56:28 -0500

    twitter metadata

--- client/src/LandingPage/index.js
+++ client/src/LandingPage/index.js
@@ -58,9 +58,10 @@ class LandingPage extends Component {
       <div className="LandingPage">
         <Helmet>
           <meta charSet="utf-8" />
-          <title>The Rotation | Men's Streetwear &amp; Designer Rent the Runway</title>
+          <title>The Rotation | Men's Rent the Runway for Streetwear &amp; Designer</title>
           <meta name="description" content="The Rotation provides its members with exclusive access to a closet full of designer clothes &amp; streetwear. Our plans give you the ability to wear multiple styles at a time for as long as you want. Some people call us Rent the Runway for Men's Streetwear, but we believe our identity is in our style selection and curation, not gender lines."></meta>
           <meta property="og:image" content={BannerImage}></meta>
+          <meta name="twitter:site" content="@therotation.club"></meta>
         </Helmet>
         <header className="background flex align_center">
           <div className="left13pct">
