b914d80ad42755e390af9e054666d250cf90125d
Zachary Podbela <podbelaapps@gmail.com>
2020-02-12 10:31:35 -0500

    New script to import item and unit CSVs. Items and units for 2.11 import

---
+++ app/scripts/inventory_import.rb
@@ -0,0 +1,53 @@
+module Scripts
+  class InventoryImport
+    require 'csv'
+  
+    def self.load_items(file_path)
+      success = 0
+      output = ""
+      
+      CSV.foreach(file_path, headers: true, header_converters: :symbol) do |line|
+        i = Item.new(line.to_hash)
+        i.virtual_qty = 0
+           
+        if i.save
+          success += 1
+        else
+          output << "Failed to create item: #{line.to_hash}\n"          
+        end
+      end
+      
+      output << "Succesfully added #{success} items."
+      puts output
+    end
+    
+    def self.load_units(file_path)
+      success = 0
+      output = ""
+      
+      CSV.foreach(file_path, headers: true, header_converters: :symbol) do |line|
+        item_keyset = [:title, :subtitle, :color, :supplier_color]
+        unit, item = line.to_hash.partition {|k,v| item_keyset.include? k }
+        unit = Hash[unit]
+        item = Hash[item]
+        
+        i = Item.where(item).take      
+        if i.nil?
+          output << "Failed to find item: #{line.to_hash}\n"   
+          
+        else
+          u = Unit.new(unit)
+          u.item = i
+          if u.save
+            success += 1
+          else
+            output << "Failed to create unit: #{line.to_hash}\n"
+          end
+        end
+      end
+      
+      output << "Succesfully added #{success} items."
+      puts output
+    end
+  end
+end
