85458c668e170c53d29b138634a2fec27fe58185
Zachary Podbela <podbelaapps@gmail.com>
2020-02-20 20:43:51 -0500

    Introduced first common UI component, RTUIFormInput

--- client/src/LoginPane/index.js
+++ client/src/LoginPane/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react"
+import RTUIFormInput from "../RTUIFormInput"
 
 class LoginPane extends Component {
   render(){
@@ -6,14 +7,8 @@ class LoginPane extends Component {
       <div className="LoginPane">
         <div className="width300 margin_auto top20 druk_small rotation_gray">Log In</div>
         <form onSubmit={this.props.handleLoginSubmit}>
-          <div className="input_box gray_border width300 height50 margin_auto top15">
-            <div className="proxima_small medium very_light_gray left20 top5 height15">Email address</div>
-            <input className="proxima_xl medium rotation_gray width260 left20" name="loginEmail" value={this.props.loginEmail} onChange={this.props.handleInputChange} />
-          </div>
-          <div className="input_box gray_border width300 height50 margin_auto top20">
-            <div className="proxima_small medium very_light_gray left20 top5 height15">Password</div>
-            <input type="password" className="proxima_xl medium rotation_gray width260 left20" name="loginPassword" value={this.props.loginPassword} onChange={this.props.handleInputChange} />
-          </div>
+          <RTUIFormInput title="Email address" name="loginEmail" value={this.props.loginEmail} onChange={this.props.handleInputChange}/>
+          <RTUIFormInput title="Password" name="loginPassword" value={this.props.loginPassword} onChange={this.props.handleInputChange} type="password"/>
           <div className="proxima_xs medium rotation_gray top10 text_center underline cursor_pointer" onClick={this.props.forgotPassword}>Forgot your password?</div>
           <input type="submit" value="Log In" className="input_box rotation_gray_border rotation_gray_background width300 height50 margin_auto top20 flex justify_center align_center proxima_xs white uppercase semibold spacing40 cursor_pointer" />
         </form>
--- client/src/SignUpPane/index.js
+++ client/src/SignUpPane/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import { Link } from "react-router-dom"
+import RTUIFormInput from "../RTUIFormInput"
 
 class SignUpPane extends Component {
   render(){
@@ -7,18 +8,9 @@ class SignUpPane extends Component {
       <div className="SignUpPane">
         <div className="width300 margin_auto top20 druk_small rotation_gray">Sign Up</div>
         <form onSubmit={this.props.handleSignUp}>
-          <div className="input_box gray_border width300 height50 margin_auto top15">
-            <div className="proxima_small medium very_light_gray left20 top5 height15">Email address</div>
-            <input className="proxima_xl medium rotation_gray width260 left20" name="registerEmail" value={this.props.registerEmail} onChange={this.props.handleInputChange} />
-          </div>
-          <div className="input_box gray_border width300 height50 margin_auto top20">
-            <div className="proxima_small medium very_light_gray left20 top5 height15">Password</div>
-            <input type="password" className="proxima_xl medium rotation_gray width260 left20" name="registerPassword" value={this.props.registerPassword} onChange={this.props.handleInputChange} />
-          </div>
-          <div className="input_box gray_border width300 height50 margin_auto top20">
-            <div className="proxima_small medium very_light_gray left20 top5 height15">Confirm Password</div>
-            <input type="password" className="proxima_xl medium rotation_gray width260 left20" name="registerConfirmPassword" value={this.props.registerConfirmPassword} onChange={this.props.handleInputChange} />
-          </div>
+          <RTUIFormInput title="Email address" name="registerEmail" value={this.props.registerEmail} onChange={this.props.handleInputChange}/>
+          <RTUIFormInput title="Password" name="registerPassword" value={this.props.registerPassword} onChange={this.props.handleInputChange} type="password"/>
+          <RTUIFormInput title="Confirm Password" name="registerConfirmPassword" value={this.props.registerConfirmPassword} onChange={this.props.handleInputChange} type="password"/>
           <div className="proxima_xs medium rotation_gray top10 text_center">By registering, I accept the <Link to="/terms" className="underline cursor_pointer">Terms of Service</Link> and <Link to="/privacy" className="underline cursor_pointer">Privacy Policy</Link></div>
           <input type="submit" value="Sign Up" className="input_box rotation_gray_border rotation_gray_background width300 height50 margin_auto top20 flex justify_center align_center proxima_xs white uppercase semibold spacing40 cursor_pointer" />
         </form>
