7192833c07278af63528375849b601d7e8494ca4
Barron Roth <barronlroth@gmail.com>
2020-02-26 14:54:14 -0500

    remove side padding class from product detail elements

--- client/src/ItemActionPane/index.js
+++ client/src/ItemActionPane/index.js
@@ -186,10 +186,10 @@ class ItemActionPane extends Component {
     
     return (
       <div className="ItemActionPane height_full" style={{position: "relative"}}>
-        <div className="modal_brand proxima_small rotation_gray opacity6 uppercase padding_sides50">{selectedItem.title.value}</div>
-        <div className="modal_description overflow_scroll druk_medium rotation_gray line_height24 padding_sides50 capitalize">{selectedItem.subtitle}</div>
+        <div className="modal_brand proxima_small rotation_gray opacity6 uppercase">{selectedItem.title.value}</div>
+        <div className="modal_description overflow_scroll druk_medium rotation_gray line_height24 capitalize">{selectedItem.subtitle}</div>
         {/* description should be fixed position from top of container */}
-        <div className="product_description proxima_small overflow_scroll rotation_gray padding_sides50">{selectedItem.description}</div>
+        <div className="product_description proxima_small overflow_scroll rotation_gray">{selectedItem.description}</div>
         {/* TODO - ADD COLOR AND CATEGORY */}
         {/* Buttons should be fixed position from bottom of container */}
         {this.props.auth && this.state.subscription ? (
