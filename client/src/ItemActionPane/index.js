8fd674753a64de1620fedf55903d0d1b5b92b3f3
Barron Roth <barronlroth@gmail.com>
2020-02-26 19:24:05 -0500

    additional css fixes for reserved state

--- client/src/ItemActionPane/index.js
+++ client/src/ItemActionPane/index.js
@@ -191,7 +191,7 @@ class ItemActionPane extends Component {
         <div className="product_description proxima_small overflow_scroll rotation_gray">{selectedItem.description}</div>
         {this.props.auth && this.state.subscription ? (
           <div>
-            <div className="modal_size_btns flex top40 justify_between">
+            <div className="modal_size_btns flex modal_buttons">
               {this.state.modalSizes.forEach(size => {
                 size.available = selectedItem.sizes[size.value] > 0
               })}
@@ -222,7 +222,7 @@ class ItemActionPane extends Component {
                 })
               )}
             </div>
-            <div className="modal_buttons flex justify_between top40">
+            <div className="modal_buttons flex">
               {myRotationItemSelected ? (
                 <div className="reserve_btn rotation_gray_border proxima_medium rotation_gray spacing10 flex justify_center align_center uppercase cursor_pointer green request_to_buy" onClick={(e) => this.requestToBuy(e)}>Request to Buy</div>
               ) : upNextItemSelected ? (
