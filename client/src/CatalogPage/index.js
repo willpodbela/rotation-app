8e6f1df760bfec6265c9b2297bb0120180d1cdd5
Barron Roth <barronlroth@gmail.com>
2020-03-06 14:23:53 -0500

    almost done with cta banner

--- client/src/CatalogPage/index.js
+++ client/src/CatalogPage/index.js
@@ -179,6 +179,14 @@ class CatalogPage extends Component {
     return (
       <div className="CatalogPage flex justify_center align_center gray_border_top padding_bottom300">
         <RotationHelmet title = "Clothing | The Rotation" />
+        <div className="section cta">
+          <div className="container w-container">
+            <div className="cta-row w-row">
+              <div className="w-col w-col-6 w-col-stack"></div>
+              <div className="w-col w-col-6 w-col-stack"></div>
+            </div>
+          </div>
+        </div>
         <div className="catalog_wrapper padding_top25 flex sides13pct">
           <div className="filters_and_designers width150 padding_right10">
             <div className="fixed_sidebar overflow_scroll width150">
