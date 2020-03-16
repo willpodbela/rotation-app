6dc2092c308bcf89e596017c0960599f950ba75c
Zachary Podbela <podbelaapps@gmail.com>
2020-03-16 14:37:11 -0400

    Changed layout to virtical when in mobile for item modal

--- client/src/ItemModal/index.js
+++ client/src/ItemModal/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import "./style.css"
 import Modal from "react-bootstrap/Modal"
 import Auth from "../modules/Auth"
 import ItemActionPane from "../ItemActionPane"
@@ -16,21 +17,43 @@ class ItemModal extends Component {
   render(){
     return (
       <Modal show={this.props.show || true} dialogClassName="modal_item" centered>
-        <div className="flex">
-          <div className="modal_section overflow_hidden height500 width_half light_background flex justify_center align_center">
-            <img className="modal_image blend_background" src={this.props.item.image_url} alt="" />
+        <div className="non_mobile_item_modal">
+          <div className="flex">
+            <div className="modal_section overflow_hidden height500 width_half light_background flex justify_center align_center">
+              <img className="modal_image blend_background" src={this.props.item.image_url} alt="" />
+            </div>
+            <div className="modal_section height500 width_half white_background">
+              <FontAwesomeIcon className="close_btn rotation_gray font20 float_right padding_top20 padding_bottom20 padding_sides25 cursor_pointer" onClick={this.props.onClose} icon="times" />
+              <div className="top50">
+                <ItemActionPane
+                  item={this.props.item}
+                  auth={this.props.auth}
+                  userLoggedIn={this.props.userLoggedIn}
+                  apiResponseHandler={this.props.apiResponseHandler}
+                  showOnboardingModal={this.props.showOnboardingModal}
+                  actionComplete={this.props.actionComplete}
+                />
+              </div>
+            </div>
           </div>
-          <div className="modal_section height500 width_half white_background">
-            <FontAwesomeIcon className="close_btn rotation_gray font20 float_right padding_top20 padding_bottom20 padding_sides25 cursor_pointer" onClick={this.props.onClose} icon="times" />
-            <div className="top50">
-              <ItemActionPane
-                item={this.props.item}
-                auth={this.props.auth}
-                userLoggedIn={this.props.userLoggedIn}
-                apiResponseHandler={this.props.apiResponseHandler}
-                showOnboardingModal={this.props.showOnboardingModal}
-                actionComplete={this.props.actionComplete}
-              />
+        </div>
+        <div className="mobile_item_modal">
+          <div className="flex modal_section">
+            <div className="light_background flex justify_center align_center">
+              <img className="modal_image blend_background" src={this.props.item.image_url} alt="" />
+            </div>
+            <div className="white_background">
+              <FontAwesomeIcon className="close_btn rotation_gray font20 float_right padding_top20 padding_bottom20 padding_sides25 cursor_pointer" onClick={this.props.onClose} icon="times" />
+              <div className="top50">
+                <ItemActionPane
+                  item={this.props.item}
+                  auth={this.props.auth}
+                  userLoggedIn={this.props.userLoggedIn}
+                  apiResponseHandler={this.props.apiResponseHandler}
+                  showOnboardingModal={this.props.showOnboardingModal}
+                  actionComplete={this.props.actionComplete}
+                />
+              </div>
             </div>
           </div>
         </div>
