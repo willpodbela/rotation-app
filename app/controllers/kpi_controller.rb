05026a4f050513881d2c40cc5b304b45b09e5360
Zachary Podbela <podbelaapps@gmail.com>
2020-03-24 21:14:44 -0400

    added includes to avoid N queries on database

--- app/controllers/kpi_controller.rb
+++ app/controllers/kpi_controller.rb
@@ -13,7 +13,7 @@ class KpiController < ApplicationController
     @penetration_items = @penetration_items.sort_by { |k, v| v.max_by(&:created_at).created_at }.reverse!
     
     @customer_sizes = Hash.new
-    User.paying_customers.each do |u|
+    User.paying_customers.includes(:reservations).each do |u|
       res_sizes = u.reservations.map(&:size).compact
       size_freq = Hash[res_sizes.group_by{|x|x}.map{|k, v| [k, 1.0*v.size/res_sizes.length]}]
       @customer_sizes.merge!(size_freq) {|k, o, n| o + n}
