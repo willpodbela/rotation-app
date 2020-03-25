490632e38f84a087f8f49a8198deb6045661ff4e
Zachary Podbela <podbelaapps@gmail.com>
2020-03-25 16:53:31 -0400

    Alphabetized filter options

--- client/src/RTUIFilterSidebar/index.js
+++ client/src/RTUIFilterSidebar/index.js
@@ -17,17 +17,18 @@ class RTUIFilterSidebar extends Component {
   }
   
   dressOptions(options) {
+    var ret = []
     if(options.constructor === Object) {
-      var ret = []
       for (var opt in options) {
         ret.push({value: opt, selected: false, children: this.dressOptions(options[opt])})
       }
-      return ret
     } else {
-      return options.map(option => {
+      ret = options.map(option => {
         return {value: option, selected: false, children: null}
       })
     }
+    ret.sort((a, b) => (a.value > b.value) ? 1 : ((b.value > a.value) ? -1 : 0));
+    return ret
   }
   
   // returns an array, first value is the new options true, second value is wether or not the value was found in that tree
