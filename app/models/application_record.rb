b67c3170a79af3e93820f3f0779d0fb1db917986
Zachary Podbela <podbelaapps@gmail.com>
2020-04-21 19:31:40 -0400

    Refactored aircloneable

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,26 +1,28 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
   
-  # TODO: Can we move this logic into the air_clone module somehow? Similar to how
-  # "devise :confirmable" also users to add extra functionality to a model.
-  def self.aircloneable
-    self.define_method(:airclone) do
-      "AirClone::#{self.class.name}".constantize.find_or_create_with_rails_record(self)
-    end
-    
-    self.set_callback :save, :after do
-      begin
-        self.airclone.sync
-      rescue 
-        # Silently fail all AirClone Errors
+  class << self
+    # TODO: Can we move this logic into the air_clone module somehow? Similar to how
+    # "devise :confirmable" also users to add extra functionality to a model.
+    def aircloneable
+      define_method(:airclone) do
+        "AirClone::#{self.class.name}".constantize.find_or_create_with_rails_record(self)
       end
-    end
-    
-    self.set_callback :destroy, :after do
-      begin
-        self.airclone.destroy
-      rescue 
-        # Silently fail all AirClone Errors
+  
+      set_callback :save, :after do
+        begin
+          self.airclone.sync
+        rescue 
+          # Silently fail all AirClone Errors
+        end
+      end
+  
+      set_callback :destroy, :after do
+        begin
+          self.airclone.destroy
+        rescue 
+          # Silently fail all AirClone Errors
+        end
       end
     end
   end
