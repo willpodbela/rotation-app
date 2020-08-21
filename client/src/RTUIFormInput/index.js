6acc0498ff6ea036407c8571dfbeefd2cc3554dd
Michelle <bergerm613@gmail.com>
2020-08-21 10:47:44 -0400

    removed extra promo button

--- client/src/RTUIFormInput/index.js
+++ client/src/RTUIFormInput/index.js
@@ -6,7 +6,7 @@ class RTUIFormInput extends Component {
     return (
       <div className={"input_box gray_border width"+(this.props.width || "300")+" height50 top20"}>
         <div className="proxima_small medium very_light_gray left20 top5 height15">{this.props.title}</div>
-        <InputMask className="input_field proxima_xl medium rotation_gray width260 left20 gray_placeholder nopadding_left" type={this.props.type || "text"} name={this.props.name} value={this.props.value} placeholder={this.props.placeholder} onChange={this.props.onChange} mask={this.props.mask} maskChar={this.props.maskChar} />
+        <InputMask className={"input_field proxima_xl medium rotation_gray width"+ (this.props.width - 40 || "260") + " left20 gray_placeholder nopadding_left"} type={this.props.type || "text"} name={this.props.name} value={this.props.value} placeholder={this.props.placeholder} onChange={this.props.onChange} mask={this.props.mask} maskChar={this.props.maskChar} />
       </div>
     )
   }
