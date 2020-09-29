eebbd91abf1acd62bfa7290bcd9ed42f75ef4473
Zachary Podbela <podbelaapps@gmail.com>
2020-09-28 23:19:09 -0400

    New shipping tacking info email

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,4 +1,8 @@
 class Shipment < ApplicationRecord
     has_and_belongs_to_many :reservations
     
+    after_create do |shipment|
+        # We'll use Active Record Callbacks to send fulfillment notification emails to the team
+        ShippingConfirmationMailer.with(shipment: shipment).shipment_created.deliver
+    end
 end
