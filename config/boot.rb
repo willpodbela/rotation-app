730b060dce7e827e382a51c836a1dd97a4fd595b
will-podbela <willpodbela@gmail.com>
2025-12-03 16:51:36 -0500

    Move Zeitwerk ignore configuration to boot.rb before app initialization

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,11 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+# Configure Zeitwerk to skip directories with explicit module namespacing
+if defined?(Zeitwerk)
+  Zeitwerk::Loader.for_gem.ignore("#{__dir__}/../app/queries")
+  Zeitwerk::Loader.for_gem.ignore("#{__dir__}/../app/scripts")
+  Zeitwerk::Loader.for_gem.ignore("#{__dir__}/../app/services")
+end
+
--- config/initializers/autoload_queries.rb
+++
@@ -1,8 +0,0 @@
-# Configure Zeitwerk to skip certain directories where classes use explicit module namespacing
-# These will be lazy-loaded when first referenced
-if defined?(Zeitwerk)
-  loader = Rails.autoloaders.main
-  loader.ignore("#{Rails.root}/app/queries")
-  loader.ignore("#{Rails.root}/app/scripts")
-  loader.ignore("#{Rails.root}/app/services")
-end
